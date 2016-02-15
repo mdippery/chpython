a1a1c89fc02161493909c07065f8cedb78968328
Michael Dippery <michael@monkey-robot.com>
2016-02-15 14:45:52 -0800

    Skeleton of `chpython` function

--- share/chpython/chpython.sh
+++ share/chpython/chpython.sh
@@ -2,3 +2,20 @@ CHPYTHON_VERSION='0.1.0'
 PYTHONS=()
 
 [ -d "${HOME}/.pythons" ] && PYTHONS+=("${HOME}/.pythons"/*)
+
+function chpython {
+  case "$1" in
+    -h|--help)
+      echo 'usage: chpython [PYTHON|VERSION|system]'
+      ;;
+    -V|--version)
+      echo "chpython $CHPYTHON_VERSION"
+      ;;
+    system)
+      echo 'TODO: Reset to system...'
+      ;;
+    *)
+      echo 'TODO: Set version...'
+      ;;
+  esac
+}
