32f722d75573139905c85fa53e64308f1a46cd07
Michael Dippery <michael@monkey-robot.com>
2016-02-15 14:50:23 -0800

    Implement `chpython_reset` function

--- share/chpython/chpython.sh
+++ share/chpython/chpython.sh
@@ -3,6 +3,12 @@ PYTHONS=()
 
 [ -d "${HOME}/.pythons" ] && PYTHONS+=("${HOME}/.pythons"/*)
 
+function chpython_reset {
+  [ -z "$CHPYTHON_ROOT" ] && return
+  PATH=${PATH//:$CHPYTHON_ROOT\/bin:/:}
+  type python
+}
+
 function chpython {
   case "$1" in
     -h|--help)
@@ -12,7 +18,7 @@ function chpython {
       echo "chpython $CHPYTHON_VERSION"
       ;;
     system)
-      echo 'TODO: Reset to system...'
+      chpython_reset
       ;;
     *)
       echo 'TODO: Set version...'
